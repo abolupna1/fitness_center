283e678cbdf143b981fd36e9e5b0f2c491ad7178
edom <abolupna1@gmail.com>
2023-11-04 06:13:54 +0300

    first commit

---
+++ fitness_center/fitness_center/doctype/customer_records/customer_records.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, edom ibrahim and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Customer Records', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ fitness_center/fitness_center/doctype/customer_subscriptions/customer_subscriptions.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2023, edom ibrahim and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Customer Subscriptions', {
+	 refresh: function(frm) {
+
+		frm.set_df_property('invoice_number', 'only_select', true);
+
+
+	}
+});
+
+
