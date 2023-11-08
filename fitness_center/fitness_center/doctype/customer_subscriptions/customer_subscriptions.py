506e1d404c43030e5bea74bf0ca8c533a2e3bb70
edom <abolupna1@gmail.com>
2023-11-08 19:23:04 +0300

    last2 commitd

--- fitness_center/fitness_center/doctype/customer_subscriptions/customer_subscriptions.py
+++ fitness_center/fitness_center/doctype/customer_subscriptions/customer_subscriptions.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2023, edom ibrahim and contributors
 # For license information, please see license.txt
 from frappe.utils import add_to_date
+from frappe.utils import today
+
 
 import frappe
 from frappe.model.document import Document
@@ -8,8 +10,12 @@ from frappe.model.document import Document
 class CustomerSubscriptions(Document):
     def before_insert(self):
         self.appointments = []
+        dates = []
         d = self.start
         for i in range(int(self.month_cont)):
+            if d > today():
+                vis={"date":d,"time":self.scheduled_time,"day":self.scheduled_day}
+                dates.append(vis)
             scd = self.append("appointments",{
                 'scheduled_date': d,
                 'scheduled_time':self.scheduled_time,
@@ -17,10 +23,32 @@ class CustomerSubscriptions(Document):
             })
             d =add_to_date(d, days=7)
         self.end=d
+        records=frappe.db.get_list('Customer Visits', filters=[
+            ['scheduled__date', 'between', [add_to_date(today(), days=1),self.end]],
+            ['status_schedule', '=', "Scheduled"],
+            ['customer_record_number', '=', self.customer_record_number],
+            ])
+        for rec in records:
+            frappe.db.delete('Customer Visits',rec['name'])
+        
+        for rec in dates:
+            vsit=frappe.new_doc("Customer Visits")
+            vsit.status = 'Subscription'
+            vsit.scheduled__date = rec['date']
+            vsit.scheduled_time = rec['time']
+            vsit.scheduled_day = rec['day']
+            vsit.status_schedule = "Scheduled"
+            vsit.customer_record_number = self.customer_record_number
+            vsit.insert()
+
     def on_update(self):
         self.appointments = []
+        dates = []
         d = self.start
         for i in range(int(self.month_cont)):
+            if d > today():
+                vis={"date":d,"time":self.scheduled_time,"day":self.scheduled_day}
+                dates.append(vis)
             scd = self.append("appointments",{
                 'scheduled_date': d,
                 'scheduled_time':self.scheduled_time,
@@ -28,6 +56,38 @@ class CustomerSubscriptions(Document):
 			})
             d =add_to_date(d, days=7)
         self.end=d
+        records=frappe.db.get_list('Customer Visits', filters=[
+            ['scheduled__date', 'between', [add_to_date(today(), days=1),self.end]],
+            ['status_schedule', '=', "Scheduled"],
+            ['customer_record_number', '=', self.customer_record_number],
+            ])
+        for rec in records:
+            frappe.db.delete('Customer Visits',rec['name'])
+        
+        for rec in dates:
+            vsit=frappe.new_doc("Customer Visits")
+            vsit.status = 'Subscription'
+            vsit.scheduled__date = rec['date']
+            vsit.scheduled_time = rec['time']
+            vsit.scheduled_day = rec['day']
+            vsit.status_schedule = "Scheduled"
+            vsit.customer_record_number = self.customer_record_number
+            vsit.insert()
+
+
+
+
+        
+
+
+
+
+  #  def after_insert(self):
+   #         records=frappe.db.get_list("Customer Records")
+   #         vsit=frappe.new_doc("Customer Visits")
+    #        vsit.status = 'Free'
+      #      vsit.customer_record_number = self.name
+         #   vsit.insert()
 
         
    
